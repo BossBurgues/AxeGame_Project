0469a76b8a2bcd07ca7492958c749d547d69ddc8
Gustavo Miguel Nicolodi <Gustavmiguel3679@gmail.com>
2025-10-16 15:22:20 -0300

    Refactor main function to include boolean comparisons and their explanations

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,9 +1,19 @@
 #include "raylib.h"
 int main() 
 {
-    float root_beer = 1.99;
-    double cheese_burger{5.99};
-    bool shouldHaveLunch{};
+    
+    bool equal{4 == 9};
+    /*Returns false because 4 is not equal to 9*/
+    bool notEqual{4 != 9};
+    /*Returns true becouse 4 is not equal to 9*/
+    bool lessThan{4 < 9};
+    /*Returns true because 4 is less than 9*/
+    bool greaterThan{4 > 9};
+    /*Returns false because 4 is not greater than 9*/
+    bool lessThanOrEqual{4 <= 9};
+    /*Returns true because 4 is less than or equal to 9*/
+    bool greaterThanOrEqual{4 >= 9};
+    /*Returns false because 4 is not greater than or equal to 9*/
 
     int width;
     width = 350;
