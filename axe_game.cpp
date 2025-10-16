ab3eed54023b79859948dd26a536fc8a11a5c5a9
Gustavo Miguel Nicolodi <Gustavmiguel3679@gmail.com>
2025-10-16 11:39:10 -0300

    Addressed concepts of debugging and observable values.

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,6 +1,10 @@
 #include "raylib.h"
 int main() 
 {
+    float root_beer = 1.99;
+    double cheese_burger{5.99};
+    bool shouldHaveLunch{};
+
     int width;
     width = 350;
     InitWindow(width, 600, "Axe Game");
